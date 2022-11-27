1ddf17e3c75e8591a80a3a1153009051e267f432
daichi kouno <15214kouno@yuai.ac.jp>
2022-11-27 10:28:09 +0900

    indexに順番を投稿順に変更、投稿時のリダイレクト先をindexに変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   def index
     @q = Post.ransack(params[:q])
-    @posts = @q.result.includes(:user).page(params[:page])
-    @post = current_user.posts.new   
+    @posts = @q.result.includes(:user).page(params[:page]).order(created_at: :desc)
+    @post = current_user.posts.new
   end
 
   def show
@@ -20,6 +20,8 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create!(post_params)
+    redirect_to posts_path
+
     # @post = current_user.posts.new(post_params)
     # if @post.save
     #   redirect_back(fallback_location: posts_path)
