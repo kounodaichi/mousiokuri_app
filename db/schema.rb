d6cf6b09baf55905bde1b7e2c799b11c4796dd99
daichi kouno <15214kouno@yuai.ac.jp>
2023-02-09 11:02:41 +0900

    task/indexにてuser.emailを表示

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2023_01_26_171636) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
+    t.string "title" 
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
