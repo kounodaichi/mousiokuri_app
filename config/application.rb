d02aaf08418f5e9dce407fd4c7e2096dcbe817c0
daichi kouno <15214kouno@yuai.ac.jp>
2023-02-28 20:54:37 +0900

    assets/stylesheets/application.cssのbootstrapの記述を消去

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module PostCalendar1App
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
   end
 end
